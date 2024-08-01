de53b5bcda0f4343d46384c9c94d866b59a86f55
matteoscaringi <me@matteoscaringi.be>
2024-08-01 19:34:37 +0200

    fix repo

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-<<<<<<< HEAD
   import Greeting from "./lib/Greeting.svelte";
   import About from "./lib/About.svelte";
   import Navbar from "./lib/Navbar.svelte";
@@ -16,18 +15,3 @@
     font-weight: 500;
   }
 </style>
-=======
-  import Greeting from './lib/Greeting.svelte';
-</script>
-
-<main>
- <Greeting/>
-</main>
-
-<style lang="scss">
-  main{
-    padding: none;
-    margin: none;
-  }
-</style>
->>>>>>> 61adba16fa8dd676ea8762b57eab641e38868288
