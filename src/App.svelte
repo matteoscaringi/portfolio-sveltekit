7fef63e283e823fa939f0bbcb0a1505c4fac417c
matteoscaringi <me@matteoscaringi.be>
2024-08-01 18:00:27 +0200

    add initial files

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import Greeting from './lib/Greeting.svelte';
+</script>
+
+<main>
+ <Greeting/>
+</main>
+
+<style lang="scss">
+  main{
+    padding: none;
+    margin: none;
+  }
+</style>
